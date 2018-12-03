944383c2975f1990abb0d01dea17aba6c66504a4
Jameson13B <jamesonbrown@neolivity.com>
2018-12-02 20:06:15 -0700

    Updates to Location route

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../styles/Location.css";
+import MapContainer from "../Maps/MapContainer";
 
 class Location extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (
-            <div className='Location' style={{height: "60vh"}}>
-                Location Page
-                <p>This page is under construction.</p>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="Location" style={{ height: "60vh" }}>
+        <p>
+          Our 10 square feet of Texas is located on West End Road in the
+          beautiful cliffs of Negril, Jamaica. Across the street and a little
+          down from The Spa Retreat. Taxis know where we are located or you can
+          call Connie at 876-846-7890 for directions.
+        </p>
+        <p>Open Mon - Sat 9am-11pm and Sun 4pm-11pm</p>
+        <p>*Hours vary in off season</p>
+        <MapContainer />
+      </div>
+    );
+  }
 }
- 
+
 export default Location;
